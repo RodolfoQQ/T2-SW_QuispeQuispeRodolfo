09a41df8801bc4a792ad381aba2b06f13e398e9b
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-06-23 11:21:02 -0500

    primer comit

---
+++ src/main/java/cibertec/T2_SW_RodolfoQuispe/T2SwRodolfoQuispeApplication.java
@@ -0,0 +1,13 @@
+package cibertec.T2_SW_RodolfoQuispe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SwRodolfoQuispeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SwRodolfoQuispeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/T2_SW_RodolfoQuispe/T2SwRodolfoQuispeApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.T2_SW_RodolfoQuispe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2SwRodolfoQuispeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
